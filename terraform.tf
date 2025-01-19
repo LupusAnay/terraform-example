dd7828d2838b54fadb83bc2d63365355d3884fe9
Ilia Kopeshtianski <lupusanay@gmail.com>
2025-01-19 15:19:56 +0400

    Initial commit

---
+++ terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = "1.10.4"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.84"
+    }
+    random = {
+      source = "hashicorp/random",
+      version = "3.6.3"
+    }
+    archive = {
+      source = "hashicorp/archive"
+      version = "2.7.0"
+    }
+
+  }
+}
